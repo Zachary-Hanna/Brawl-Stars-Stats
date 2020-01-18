ebde123c928b55c4a3901b785c3f6c60727545e0
Ed <BigMac@herpderp.local>
2020-01-17 20:31:04 -0500

    fixed the thing

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
 const app = express();
+
+
 app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('docs'));
+
+app.use(express.static('public'))
