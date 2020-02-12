801bb7f19ce2ed774cd32a3d5f7efd55f008edbb
Zachary-Hanna <59712610+Zachary-Hanna@users.noreply.github.com>
2020-02-11 20:49:37 -0500

    graph change

--- express_server/public/sketch.js
+++ express_server/public/sketch.js
@@ -7,43 +7,87 @@ const populatePlayerInfo = async () => {
 
         console.log(json);
 
-        // populate DOM elements
-        document.getElementById('user').textContent = json.name;
-        document.getElementById('trophyCount').textContent = json.trophies;
-        document.getElementById('level').textContent = json.expLevel;
-        document.getElementById('club').textContent = json.club.name;
+        createGraphs(json);
+
+
+
+
     } catch (error) {
         console.log(error);
     }
 }
 
 // example graph created in this fucntion
-const createGraphs = () => {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
+const createGraphs = (json) => {
+    var ctx = 'myChart2';
 
-        // The data for our dataset
+    var gold = 'rgb(213, 197, 21)'
+    var silver = 'rgb(183, 182, 178)'
+    var bronze = 'rgb(191, 128, 27)'
+
+    var chart = new Chart(ctx, {
+        type: 'doughnut',
         data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45]
-            }]
+                data: [
+                    json["3vs3Victories"],
+                    json.soloVictories,
+                    json.duoVictories
+                ],
+
+                backgroundColor: [
+                    `${gold}`,
+                    `${silver}`,
+                    `${bronze}`
+                ],
+                borderColor: [
+                    `${gold}`,
+                    `${silver}`,
+                    `${bronze}`
+                ],
+            }],
+
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                '3v3 Victories',
+                'Solo Victories',
+                'Duo Victories'
+            ],
+
         },
 
-        // Configuration options go here
-        options: {}
+
+
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+
+        }
+
     });
+    chart.canvas.parentNode.style.height = '500px';
+    chart.canvas.parentNode.style.width = '500px';
 }
 
-console.log("hiii");
+
+
+
+
+
+
+
+
+
 
 populatePlayerInfo();
-createGraphs();
+
 
 
 var input = document.getElementById("playertag").value;
