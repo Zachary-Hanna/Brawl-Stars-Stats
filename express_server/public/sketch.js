6b971102c0f6fc09de67a2bc001638f5dde250bb
Ed <BigMac@herpderp.local>
2020-02-11 20:49:34 -0500

    Added input boxes

--- express_server/public/sketch.js
+++ express_server/public/sketch.js
@@ -1,6 +1,6 @@
 // gets players info from server
-const populatePlayerInfo = async () => {
-    const playerID = '9U0Q8VUR' //placeholder for now
+const populatePlayerInfo = async (playerID) => {
+
     try {
         const response = await fetch(`/player/${playerID}`);
         const json = await response.json();
@@ -17,34 +17,13 @@ const populatePlayerInfo = async () => {
     }
 }
 
-// example graph created in this fucntion
-const createGraphs = () => {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-
-        // The data for our dataset
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45]
-            }]
-        },
-
-        // Configuration options go here
-        options: {}
-    });
+//Sample: 9U0Q8VUR
+function onClick() {
+    populatePlayerInfo(document.getElementById('playerTag').value);
 }
 
-console.log("hiii");
-
-populatePlayerInfo();
-createGraphs();
 
 
-var input = document.getElementById("playertag").value;
-console.log(input);
+
+
+
