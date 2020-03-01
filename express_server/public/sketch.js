5bd82fb91bfa33621a1d54eeec8ac515c198dd14
Ed <BigMac@herpderp.local>
2020-02-29 22:18:10 -0500

    trying out bootstrap cards

--- express_server/public/sketch.js
+++ express_server/public/sketch.js
@@ -25,7 +25,6 @@ const createGraphs = (json) => {
                     json.soloVictories,
                     json.duoVictories
                 ],
-
                 backgroundColor: [
                     `${gold}`,
                     `${silver}`,
@@ -37,19 +36,86 @@ const createGraphs = (json) => {
                     `${bronze}`
                 ],
             }],
-
             // These labels appear in the legend and in the tooltips when hovering different arcs
             labels: [
                 '3v3 Victories',
                 'Solo Victories',
                 'Duo Victories'
             ],
-
         },
         options: {}
     });
-    victoryChart.canvas.parentNode.style.height = '500px';
-    victoryChart.canvas.parentNode.style.width = '500px';
+
+    /* 3 donut charts */
+    var donutOptions = {
+        cutoutPercentage: 85,
+        legend: {
+            position: 'bottom',
+            padding: 5,
+            labels: {
+                pointStyle: 'circle',
+                usePointStyle: true
+            }
+        }
+    };
+
+    var donutColors = ['#007bff','#28a745','#333333']
+
+    // donut 1
+    var chDonutData1 = {
+        labels: ['Bootstrap', 'Popper', 'Other'],
+        datasets: [{
+            backgroundColor: donutColors,
+            borderWidth: 0,
+            data: [74, 11, 40]
+        }]
+    };
+
+    var chDonut1 = document.getElementById("chDonut1");
+    if (chDonut1) {
+        new Chart(chDonut1, {
+            type: 'pie',
+            data: chDonutData1,
+            options: donutOptions
+        });
+    }
+
+    // donut 2
+    var chDonutData2 = {
+        labels: ['Wips', 'Pops', 'Dags'],
+        datasets: [{
+            backgroundColor: donutColors,
+            borderWidth: 0,
+            data: [40, 45, 30]
+        }]
+    };
+    var chDonut2 = document.getElementById("chDonut2");
+    if (chDonut2) {
+        new Chart(chDonut2, {
+            type: 'pie',
+            data: chDonutData2,
+            options: donutOptions
+        });
+    }
+
+    // donut 3
+    var chDonutData3 = {
+        labels: ['Angular', 'React', 'Other'],
+        datasets: [{
+            backgroundColor: donutColors,
+            borderWidth: 0,
+            data: [21, 45, 55]
+        }]
+    };
+    var chDonut3 = document.getElementById("chDonut3");
+    if (chDonut3) {
+        new Chart(chDonut3, {
+            type: 'pie',
+            data: chDonutData3,
+            options: donutOptions
+        });
+    }
+
 
 
 
@@ -84,10 +150,10 @@ const createGraphs = (json) => {
     });
 
 }
-    populatePlayerInfo('9U0Q8VUR');
+populatePlayerInfo('9U0Q8VUR');
 
-    //Sample: 9U0Q8VUR
-    function onClick() {
-        populatePlayerInfo(document.getElementById('playerTag').value);
-    }
+//Sample: 9U0Q8VUR
+function onClick() {
+    populatePlayerInfo(document.getElementById('playerTag').value);
+}
 
